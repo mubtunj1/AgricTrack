6c212b255be17e56a3370bfe7c07242b9b8f733f
Divinelewis <divinitysoft0@gmail.com>
2023-07-12 21:17:49 +0100

    updated the routes path for the weather update

--- agrictrack/routes.py
+++ agrictrack/routes.py
@@ -3,7 +3,7 @@ from agrictrack.forms import RegistrationForm, LoginForm
 from agrictrack import app, db, bcrypt
 from agrictrack.models import User
 from flask_login import login_user, current_user, logout_user, login_required
-
+import requests
 
 
 @app.route("/")
@@ -56,27 +56,27 @@ def logout():
 def dashboard():
     return render_template('dashboard.html')
 
+
 @app.route('/weatherupdate', methods=['GET', 'POST'])
-def weather_report():
+@login_required
+def weatherupdate():
     if request.method == 'POST':
-        city = request.form.get('city')
-        api_key = 'cfeacba5d60826e68db04677b8ac9173'
-
-        # Make a request to the weather API
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
-        response = requests.get(url)
-        data = response.json()
-
-        if response.status_code == 200:
-            # Parse the required weather data from the API response
-            weather_description = data['weather'][0]['description']
-            temperature = data['main']['temp']
-            humidity = data['main']['humidity']
-
-            # Render the weather report template with the fetched data
-            return render_template('weatherupdate.html', city=city, description=weather_description, temperature=temperature, humidity=humidity)
+        city = request.form['city']
+        weather_data = get_weather_data(city)
+        if weather_data:
+            return render_template('weatherupdate.html', city=city, weather_data=weather_data)
         else:
-            error_message = data['message']
-            return render_template('weatherupdate.html', error=error_message)
-
+            error_message = f"Failed to fetch weather data for {city}. Please try again."
+            return render_template('weatherupdate.html', error_message=error_message)
     return render_template('weatherupdate.html')
+
+def get_weather_data(city):
+    api_key = 'cfeacba5d60826e68db04677b8ac9173'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather_data = response.json()
+        temperature = weather_data['main']['temp']
+        condition = weather_data['weather'][0]['description']
+        return {'temperature': temperature, 'condition': condition}
+    return None
